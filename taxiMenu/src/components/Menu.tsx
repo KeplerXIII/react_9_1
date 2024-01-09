f02dd3873d90f3717e07ab7a33c96c14c7fad7b2
keplerxiii <a.v.pushkin@gmail.com>
2024-01-09 16:18:05 +0300

    upd

--- taxiMenu/src/components/Menu.tsx
+++ taxiMenu/src/components/Menu.tsx
@@ -1,38 +1,21 @@
 import { NavLink } from 'react-router-dom'
 
 export const Menu = () => {
+  const menuItemClassName = ({ isActive }: { isActive: boolean }) =>
+    isActive ? 'menu__item menu__item-active' : 'menu__item'
+
   return (
     <nav className='menu'>
-      <NavLink
-        className={({ isActive }) =>
-          isActive ? 'menu__item menu__item-active' : 'menu__item'
-        }
-        to='/'
-      >
+      <NavLink className={menuItemClassName} to='/'>
         Главная
       </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          isActive ? 'menu__item menu__item-active' : 'menu__item'
-        }
-        to='/drift'
-      >
+      <NavLink className={menuItemClassName} to='/drift'>
         Дрифт-такси
       </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          isActive ? 'menu__item menu__item-active' : 'menu__item'
-        }
-        to='/timeattack'
-      >
+      <NavLink className={menuItemClassName} to='/timeattack'>
         Time Attack
       </NavLink>
-      <NavLink
-        className={({ isActive }) =>
-          isActive ? 'menu__item menu__item-active' : 'menu__item'
-        }
-        to='/forza'
-      >
+      <NavLink className={menuItemClassName} to='/forza'>
         Forza Karting
       </NavLink>
     </nav>
