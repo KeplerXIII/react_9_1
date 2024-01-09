473b0e52160a177da48ac1d21ffd8a0a46ae5ad3
keplerxiii <a.v.pushkin@gmail.com>
2024-01-09 15:02:23 +0300

    completed menu

--- taxiMenu/src/App.tsx
+++ taxiMenu/src/App.tsx
@@ -1,22 +1,26 @@
 import { Route, Routes } from 'react-router-dom'
+
+import { HomePage } from './components/HomePage'
+import { DriftPage } from './components/DriftPage'
+import { ForzaPage } from './components/ForzaPage'
+import { Menu } from './components/Menu'
+import { TimeAttackPage } from './components/TimaeAttackPage'
+
 import './App.css'
 
 function App() {
   return (
-    <nav className='menu'>
-      <a className='menu__item' href='/'>
-        Главная
-      </a>
-      <a className='menu__item' href='/drift'>
-        Дрифт-такси
-      </a>
-      <a className='menu__item' href='/timeattack'>
-        Time Attack
-      </a>
-      <a className='menu__item' href='/forza'>
-        Forza Karting
-      </a>
-    </nav>
+    <div>
+      <Menu />
+      <div className='page'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/drift' element={<DriftPage />} />
+          <Route path='/timeattack' element={<TimeAttackPage />} />
+          <Route path='/forza' element={<ForzaPage />} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
--- taxiMenu/src/components/Menu.tsx
+++ taxiMenu/src/components/Menu.tsx
@@ -1,4 +1,20 @@
-export const Menu = () => {
-  return null
-}
+import { Link } from 'react-router-dom'
 
+export const Menu = () => {
+  return (
+    <nav className='menu'>
+      <Link className='menu__item' to='/'>
+        Главная
+      </Link>
+      <Link className='menu__item' to='/drift'>
+        Дрифт-такси
+      </Link>
+      <Link className='menu__item' to='/timeattack'>
+        Time Attack
+      </Link>
+      <Link className='menu__item' to='/forza'>
+        Forza Karting
+      </Link>
+    </nav>
+  )
+}
